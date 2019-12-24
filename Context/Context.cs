ba3e817643dd975819ceba860c8a52f1aedcde66
KloderGit <idzhyan.develop@outlook.com>
2019-12-24 16:33:40 +0300

    Добавьте файлы проекта.

---
+++ Context/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crm.service.database
+{
+    public class CrmDataBaseContext : DbContext
+    {
+        public DbSet<EventLink> Events { get; set; }
+
+        public CrmDataBaseContext()
+        {
+        }
+
+        public CrmDataBaseContext(DbContextOptions<CrmDataBaseContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ Context/EventLink.cs
@@ -0,0 +1,14 @@
+﻿namespace crm.service.database
+{
+    public class EventLink
+    {
+        public int Id { get; set; }
+        public int CrmKey { get; set; }
+        public System.Guid lCKey { get; set; }
+
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Department { get; set; }
+        public string PartTime { get; set; }
+    }
+}
